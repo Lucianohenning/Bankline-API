2dd177d7231ce21c38cd0a391df96cea49724489
Lucianohenning <44951713+Lucianohenning@users.noreply.github.com>
2022-05-04 18:54:29 -0300

    criação de classes de modelo da aplicação

---
+++ src/main/java/com/dio/santander/BankLine/api/model/Correntista.java
@@ -0,0 +1,36 @@
+package com.dio.santander.BankLine.api.model;
+
+public class Correntista {
+	
+	private Integer id;
+	private String Nome;
+	private String Cpf;
+	private Conta conta;
+	public Conta getConta() {
+		return conta;
+	}
+	
+	public void setConta(Conta conta) {
+		this.conta = conta;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return Nome;
+	}
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+	public String getCpf() {
+		return Cpf;
+	}
+	public void setCpf(String cpf) {
+		Cpf = cpf;
+	}
+
+}
---
+++ src/main/java/com/dio/santander/BankLine/api/model/MovimentacaoTipo.java
@@ -0,0 +1,7 @@
+package com.dio.santander.BankLine.api.model;
+
+public enum MovimentacaoTipo {
+	RECEITA,
+	DESPESA
+
+}
